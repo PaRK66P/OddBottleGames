3f8783452fceef768c67cad0d719417bc1f39a39
PaRK66P <2200637@uad.ac.uk>
2025-01-23 11:32:15 +0000

    Intellisense and proof of input

--- Assets/Developers/Patrick/PlayerManager.cs
+++ Assets/Developers/Patrick/PlayerManager.cs
@@ -4,10 +4,17 @@ using UnityEngine;
 
 public class PlayerManager : MonoBehaviour
 {
+    [SerializeField]
+    private PlayerMovement playerMovement;
+    [SerializeField]
+    private PlayerInputManager playerInputManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerInputManager.playerMovement = playerMovement;
+        playerInputManager.isInitialised = true;
+        playerInputManager.EnableInput();
     }
 
     // Update is called once per frame
--- Assets/Developers/Patrick/PlayerMovement.cs
+++ Assets/Developers/Patrick/PlayerMovement.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
+    private float horizontalInput;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,11 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        transform.position = new Vector3(transform.position.x + horizontalInput * Time.deltaTime, transform.position.y, transform.position.z);
+    }
+
+    public void SetHorizontalMovementInput(InputAction.CallbackContext context)
+    {
+        horizontalInput = context.ReadValue<float>();
     }
 }
