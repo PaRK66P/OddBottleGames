33a40f0be6761cf98b0b0c30a92b83143267b9f2
Joshua Lowe <joshua220104@icloud.com>
2025-01-24 18:29:51 +0000

    rendering text onto texture to display in viewport correctly

--- Assets/Developers/Josh/VNEditorWindow.cs
+++ Assets/Developers/Josh/VNEditorWindow.cs
@@ -2,6 +2,8 @@ using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEditor.UIElements;
 
 public class VNEditorWindow : EditorWindow
 {
@@ -9,7 +11,14 @@ public class VNEditorWindow : EditorWindow
     [SerializeField]
     private int selectedIndex = -1;
     
-    private VisualElement rightPane;
+    private VisualElement topRightPane;
+    private TextField bottomRightPane;
+
+    [SerializeField]
+    private string textFieldInput = "";
+
+    [SerializeField]
+    private int fontSize = 18;
 
     //tag as menu item
     [MenuItem("Window/UI Toolkit/Visual Novel Scene Editor")]
@@ -29,7 +38,7 @@ public class VNEditorWindow : EditorWindow
     public void CreateGUI()
     {
         //enumerate all sprites in the project
-        var allObjectGuids = AssetDatabase.FindAssets("t:Sprite");
+        var allObjectGuids = AssetDatabase.FindAssets("t:Sprite", new[] { "Assets/Developers/Josh/Sprites" } );
         var allObjects = new List<Sprite>();
         foreach (var guid in allObjectGuids)
         {
@@ -43,11 +52,31 @@ public class VNEditorWindow : EditorWindow
         var splitView = new TwoPaneSplitView(0, 250, TwoPaneSplitViewOrientation.Horizontal);
         rootVisualElement.Add(splitView);
 
-        //add a list to the left pane and a scrolling viewport to the right pane
+        //add a listview to the left pane of the split view
         var leftPane = new ListView();
         splitView.Add(leftPane);
-        rightPane = new ScrollView(ScrollViewMode.VerticalAndHorizontal);
-        splitView.Add(rightPane);
+
+        //vertical splitview for right side
+        var rightSplitView = new TwoPaneSplitView(0, 500, TwoPaneSplitViewOrientation.Vertical);
+        splitView.Add(rightSplitView);
+
+        //add scrolling viewport to the top of the right side split view
+        topRightPane = new ScrollView(ScrollViewMode.VerticalAndHorizontal);
+        rightSplitView.Add(topRightPane);
+
+        //styling for text input field
+        bottomRightPane = new TextField("Dialogue");
+        bottomRightPane.multiline = true;
+        bottomRightPane.style.whiteSpace = WhiteSpace.Normal;
+        bottomRightPane.style.minHeight = EditorGUIUtility.singleLineHeight * 10.0f;
+
+        var textInputField = bottomRightPane.Q("unity-text-input");
+        textInputField.style.height = EditorGUIUtility.singleLineHeight * 10.0f;
+        textInputField.style.whiteSpace = WhiteSpace.Normal;
+
+        rightSplitView.Add(bottomRightPane);
+        bottomRightPane.value = textFieldInput;
+        bottomRightPane.RegisterValueChangedCallback(evt => { textFieldInput = evt.newValue; });
 
         //bind the enumerated list of sprites to the left pane
         leftPane.makeItem = () => new Label();
@@ -65,7 +94,7 @@ public class VNEditorWindow : EditorWindow
     private void OnSpriteSelectionChange(IEnumerable<object> selectedItems)
     {
         //clear the right pane at the start
-        rightPane.Clear();
+        topRightPane.Clear();
 
         //check that the current sprite is valid and exists
         var enumerator = selectedItems.GetEnumerator();
@@ -74,12 +103,90 @@ public class VNEditorWindow : EditorWindow
             var selectedSprite = enumerator.Current as Sprite;
             if (selectedSprite != null)
             {
-                //if we find a sprite we create an image and add it to the right viewport
-                var spriteImage = new Image();
-                spriteImage.scaleMode = ScaleMode.ScaleToFit;
-                spriteImage.sprite = selectedSprite;
+                //create a rendertexture for display and a temporary texture
+                RenderTexture renderTexture = new RenderTexture(512, 512, 24);
+                RenderTexture.active = renderTexture;
+                GL.Clear(true, true, Color.blue);
+                Texture2D newTexture = new Texture2D(512, 512, TextureFormat.RGBA32, false);
 
-                rightPane.Add(spriteImage);
+                //copy sprite to new texture
+                Graphics.Blit(selectedSprite.texture, renderTexture);
+                newTexture.ReadPixels(new Rect(0, 0, 512, 512), 0, 0);
+                newTexture.Apply();
+
+                //create temp gameobject for the canvas and canvas scaler
+                GameObject tempGO = new GameObject("TempCanv");
+                tempGO.transform.position = Vector2.zero;
+                Canvas canvas = tempGO.AddComponent<Canvas>();
+                canvas.renderMode = RenderMode.ScreenSpaceCamera;
+                CanvasScaler canvasScaler = tempGO.AddComponent<CanvasScaler>();
+                canvasScaler.referenceResolution = new Vector2(512, 512);
+                canvasScaler.screenMatchMode = CanvasScaler.ScreenMatchMode.MatchWidthOrHeight;
+                canvasScaler.matchWidthOrHeight = 0.5f;
+                canvasScaler.scaleFactor = 1.0f;
+                canvasScaler.dynamicPixelsPerUnit = 1;
+
+                //create temporary camera and add to same game object as canvas (initialised like default unity camera)
+                GameObject camGO = new GameObject("TempCam");
+                Camera camera = camGO.AddComponent<Camera>();
+                camera.cullingMask = -1;
+                camera.orthographic = true;
+                camera.nearClipPlane = 0.3f;
+                camera.farClipPlane = 1000.0f;
+                camera.orthographicSize = 256.0f;
+                camera.targetTexture = renderTexture;
+                camera.clearFlags = CameraClearFlags.Skybox;
+                camera.backgroundColor = Color.blue;
+                camGO.transform.position = new Vector3(0, 0, -20);
+                camera.depth = -1;
+
+                //set canvas worldcamera as camera
+                canvas.worldCamera = camera;
+                
+
+                //create temporary gameobject for text as a child of the canvas object
+                GameObject textGO = new GameObject("Text");
+                textGO.transform.SetParent(canvas.transform);
+                Text textComponent = textGO.AddComponent<Text>();
+                textComponent.fontSize = fontSize;
+                textComponent.color = Color.white;
+                textComponent.font = Resources.GetBuiltinResource<Font>("LegacyRuntime.ttf");
+                textComponent.text = textFieldInput;
+                textComponent.alignment = TextAnchor.MiddleCenter;
+                
+                CanvasRenderer textCanvasrenderer = textGO.GetComponent<CanvasRenderer>();
+                textCanvasrenderer.cullTransparentMesh = true;
+
+                textGO.GetComponent<RectTransform>().position = Vector3.zero;
+
+                //RectTransform rectTransform = textGO.GetComponent<RectTransform>();
+                //rectTransform.sizeDelta = new Vector2(512, 512);
+                //rectTransform.anchoredPosition = Vector2.zero;
+                //rectTransform.anchorMin = Vector2.zero;
+                //rectTransform.anchorMax = Vector2.zero;
+
+                //update canvas
+                Canvas.ForceUpdateCanvases();
+
+                //render camera
+                camera.Render();
+
+                //read pixels and apply to texture
+                newTexture.ReadPixels(new Rect(0, 0, 512, 512), 0, 0);
+                newTexture.Apply();
+
+                //create an image to display the texture we just created
+                var image = new UnityEngine.UIElements.Image();
+                image.scaleMode = ScaleMode.ScaleToFit;
+                image.image = newTexture;
+                topRightPane.Add(image);
+                
+                //destroy temp objects and reset viewport
+                RenderTexture.active = null;
+                Object.DestroyImmediate(tempGO);
+                Object.DestroyImmediate(textGO);
+                Object.DestroyImmediate(camGO);
+                renderTexture.Release();
             }
         }
     }
