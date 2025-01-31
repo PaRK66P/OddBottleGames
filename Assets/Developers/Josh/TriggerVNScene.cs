c5289d602f0f2ea8221c334cc26588c0fcdecc84
Joshua Lowe <joshua220104@icloud.com>
2025-01-31 16:58:38 +0000

    trigger doesnt reset vn scene
    re entering trigger when visual novel section is already playing doesnt reset it

--- Assets/Developers/Josh/TriggerVNScene.cs
+++ Assets/Developers/Josh/TriggerVNScene.cs
@@ -13,9 +13,12 @@ public class TriggerVNScene : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject == GameObject.Find("PlayerProto"))
+        if (!VNSceneManager.isNovelSection)
         {
-            VNSceneManager.StartNovelSceneByName("multiple scene test");
+            if (collision.gameObject == GameObject.Find("PlayerProto"))
+            {
+                VNSceneManager.StartNovelSceneByName("multiple scene test");
+            }
         }
     }
 }
