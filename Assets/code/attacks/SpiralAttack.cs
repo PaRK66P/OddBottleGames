8a9bf31c6142ae13bf4a59d870779420f70f9997
Ayame <iris.marian12@gmail.com>
2025-02-05 20:45:08 +0000

    added spiral attack (untested)

---
+++ Assets/code/attacks/SpiralAttack.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class SpiralAttack : AttackClass
+{
+    public GameObject projectilePrefab;
+    public int projectilePerWaveNo = 6;
+    public int numberOfWaves = 10;
+    public float timeBetweenWaves = 0.2f;
+    public int angleBetweenWaves = 5;
+
+    public override void Attack(ref bool b, ref List<int> itt, ref List<float> tim)
+    {
+        if (tim.Count() == 0)
+        {
+            tim.Add(0);//timer between waves
+            itt.Add(0);//wave number
+        }
+
+        tim[0] += Time.deltaTime;
+        if (tim[0]>=timeBetweenWaves)
+        {
+            tim[0] = 0;
+            float rotStep = 360 / projectilePerWaveNo;
+            UnityEngine.Vector3 rotation = new UnityEngine.Vector3(0, 0, 0);
+            rotation.z += angleBetweenWaves * itt[0];
+
+            for (int i = 0; i < projectilePerWaveNo; i++)
+            {
+                Instantiate(projectilePrefab, gameObject.transform.position, UnityEngine.Quaternion.Euler(rotation));
+                rotation.z += rotStep;
+            }
+            itt[0]++;
+        }
+
+        if (itt[0] == numberOfWaves)
+        {
+            b = false;
+            itt.Clear();
+            tim.Clear();
+        }
+    }
+}
