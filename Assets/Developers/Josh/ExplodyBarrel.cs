75f3f3c35501261a68850c925d1d1a11adb153e6
Patrick Kopecny <2200637@uad.ac.uk>
2025-02-13 14:30:46 +0000

    Explosion Fix
    Explosion now damages enemies and player

--- Assets/Developers/Josh/ExplodyBarrel.cs
+++ Assets/Developers/Josh/ExplodyBarrel.cs
@@ -6,8 +6,10 @@ public class ExplodyBarrel : MonoBehaviour
 {
     public ObjectPoolManager objectPoolManager;
     private GameObject currentExplosion;
-    private string[] attackLayers = { "Player", "Enemy", "Enviroment" };
-    private string[] collisionLayers = { "Projectile", "EnemyAttack" };
+    private GameObject currentExplosion1;
+    [SerializeField] private LayerMask layers1;
+    [SerializeField] private LayerMask layers2;
+    private string[] collisionLayers = { "Projectile" };
     LayerMask collLayers;
 
     // Start is called before the first frame update
@@ -27,8 +29,11 @@ public class ExplodyBarrel : MonoBehaviour
     {
         
         currentExplosion = objectPoolManager.GetFreeObject("Explosion");
-        currentExplosion.GetComponent<ExplosionLogic>().InitialiseEffect(LayerMask.GetMask(attackLayers), 5,4,2,1,objectPoolManager);
+        currentExplosion.GetComponent<ExplosionLogic>().InitialiseEffect(layers1, 6,8,0.1f,1,objectPoolManager);
         currentExplosion.transform.position = this.transform.position;
+        currentExplosion1 = objectPoolManager.GetFreeObject("Explosion");
+        currentExplosion1.GetComponent<ExplosionLogic>().InitialiseEffect(layers2, 6, 8, 0.1f, 1, objectPoolManager);
+        currentExplosion1.transform.position = this.transform.position;
         Destroy(this.gameObject);
     }
 
