741ab5ac23ac0c73a9704f29abfef7387abd1ecc
MCEwan10 <61874328+MCEwan10@users.noreply.github.com>
2025-03-13 13:56:36 +0000

    More Scripting

--- Assets/Developers/Ewan/Scripts/SoundManager.cs
+++ Assets/Developers/Ewan/Scripts/SoundManager.cs
@@ -4,25 +4,33 @@ using UnityEngine;
 
 public class SoundManager : MonoBehaviour
 {
+    //Back Ground Music (PLACEHOLDER)
     private AudioSource BGM;
     private AudioClip BGMClip;
-
+    //Player Footsteps Sound
     private AudioSource PFootsteps;
     private AudioClip[] SFootstepsClips;
-
+    //Player Gun sounds
     private AudioSource PGunSFX;
     private AudioClip SGunFire;
     private AudioClip SGunCock;
     private AudioClip SGunReload;
+    //Dash Sounds
+    private AudioSource PDash;
+    private AudioClip SDash;
 
-    private AudioClip PDash;
-    private AudioClip SDashClip;
 
+    public void PlayBGM()
+    {
+        BGM.clip = BGMClip;
+        BGM.loop = true;
+        BGM.Play();
+    }
 
     public void PlayPGunFire()
     {
         //set the random pitch
-        PGunSFX.pitch = Random.Range(0.7f, 1.3f);
+        PGunSFX.pitch = Random.Range(0.8f, 1.2f);
         //play sound
         PGunSFX.PlayOneShot(SGunFire);
     }
@@ -39,5 +47,11 @@ public class SoundManager : MonoBehaviour
         PGunSFX.pitch = 1f;
         //play sound
         PGunSFX.PlayOneShot(SGunReload);
+    }
+    public void PlayPDash()
+    {
+        //set ptich
+        PDash.pitch = Random.Range(0.9f, 1.2f);
+        PDash.PlayOneShot(SDash);
     }
 }
