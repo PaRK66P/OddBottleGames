d49bdee2e7d154d75ef281011346e4208546ed94
Ewan <mcewan10.ej@gmail.com>
2025-03-17 18:34:11 +0000

    Script Stuff

--- Assets/Developers/Ewan/Scripts/SoundManager.cs
+++ Assets/Developers/Ewan/Scripts/SoundManager.cs
@@ -7,19 +7,34 @@ public class SoundManager : MonoBehaviour
     //Back Ground Music (PLACEHOLDER)
     private AudioSource BGM;
     private AudioClip BGMClip;
+
     //Player Footsteps Sound
-    private AudioSource PFootsteps;
-    private AudioClip[] SFootstepsClips;
+    private AudioSource PFootsteps; //NOTE: FIND HOW TO COMPLIE ARRAY
+    private AudioClip SFootstepsClips;
+
     //Player Gun sounds
     private AudioSource PGunSFX;
     private AudioClip SGunFire;
     private AudioClip SGunCock;
     private AudioClip SGunReload;
-    //Dash Sounds
-    private AudioSource PDash;
+    private AudioClip SGunChargeFire;
+    private AudioClip SGunChargeUp;
+
+    //Player Dash Sounds
+    private AudioSource PDash; //NOTE; FIND HOW TO COMPLIE ARRAY
+    private AudioSource PDashStatus;
     private AudioClip SDash;
+    private AudioClip SDashEmpty;
+    private AudioClip SDashReady;
 
+    //Player Other Sounds
+    private AudioSource PHit;
+    private AudioClip SHit;
 
+    public void InitialiseSFX()
+    {
+        //SGunCock.Load<AudioClip>("");
+    }
     public void PlayBGM()
     {
         BGM.clip = BGMClip;
@@ -48,10 +63,28 @@ public class SoundManager : MonoBehaviour
         //play sound
         PGunSFX.PlayOneShot(SGunReload);
     }
+    public void PlayGunChargeUp()
+    {
+        //incease pitch
+        PGunSFX.pitch += 0.15f;
+        //play sound
+        PGunSFX.PlayOneShot(SGunChargeUp);
+    }
+    public void PlayGunChargeFire()
+    {
+        //Randomise pitch
+        PGunSFX.pitch = Random.Range(0.8f,1.2f);
+        PGunSFX.PlayOneShot(SGunChargeFire);
+    }
     public void PlayPDash()
     {
         //set ptich
         PDash.pitch = Random.Range(0.9f, 1.2f);
         PDash.PlayOneShot(SDash);
+    }
+    public void PlayDashEmpty()
+    {
+        PDash.pitch = 1f;
+        PDash.PlayOneShot(SDashEmpty);
     }
 }
