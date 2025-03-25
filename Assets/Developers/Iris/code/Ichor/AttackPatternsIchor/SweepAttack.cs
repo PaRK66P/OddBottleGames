42087eb72c344bac730cb1478bdc49a1c9b3558c
2200147 <2200147@uad.ac.uk>
2025-03-25 15:45:55 +0000

    sweep attack is complete
    can spawn multiple waves, has a charge up timer, can be set to collide with walls

--- Assets/Developers/Iris/code/Ichor/AttackPatternsIchor/SweepAttack.cs
+++ Assets/Developers/Iris/code/Ichor/AttackPatternsIchor/SweepAttack.cs
@@ -10,51 +10,58 @@ public class SweepAttack : AttackClass
 
     public int projectileNo = 10;
     public float chargeUpTimer = 0.5f;
+    public float projectileSpeed = 20;
 
     public int waves = 2;
     public float timeBetweenWaves = 0.5f;
+    public float waveLifespan = 2;
 
     public float TopLimit;
     public float BottomLimit;
     public float LeftLimit;
 
+    public bool collideWithWalls = false;
+
     public override void Attack(ref bool b, ref List<int> itt, ref List<float> tim, ref ObjectPoolManager dPooler, ref GameObject callingObj)
     {
         if (tim.Count() == 0)
         {
-            tim.Add(0);
-            itt.Add(0); //[0] attack is finished
-            itt.Add(0); //[1] waves spawned
+            tim.Add(0); // [0] timer beween wave spawns
+            tim.Add(waveLifespan); // [0] timer after last wave spawn
+            itt.Add(0); // [0] waves spawned
         }
 
-        if (itt[2] < waves)
+        if (itt[0] < waves)
         {
             tim[0] -= Time.deltaTime;
             if (tim[0] <= 0)
             {
                 tim[0] = timeBetweenWaves;
 
-                UnityEngine.Vector3 pos = new UnityEngine.Vector3(TopLimit, LeftLimit, 0);
+                UnityEngine.Vector3 pos = new UnityEngine.Vector3(LeftLimit, TopLimit, 0);
                 UnityEngine.Vector3 rotation = new UnityEngine.Vector3(0, 0, 0);
 
                 //spawn a wave
                 for (int i = 0; i < projectileNo; i++)
                 {
                     GameObject obj = dPooler.GetFreeObject(projectilePrefab.name);
-                    obj.GetComponent<bossProjectile>().InstantiateComponent(ref dPooler, projectilePrefab.name, callingObj.transform.position, rotation);
+                    obj.GetComponent<SweepProjectile>().InstantiateComponent(ref dPooler, projectilePrefab.name, pos, rotation, projectileSpeed, chargeUpTimer, waveLifespan, collideWithWalls);
 
-                    pos.y += (TopLimit - BottomLimit) / projectileNo;
+                    pos.y -= (TopLimit - BottomLimit) / projectileNo;
                 }
 
-                itt[1]++;
+                itt[0]++;
             }
         }
-
-        if (itt[0] == 1)
+        else
         {
-            b = false;
-            itt.Clear();
-            tim.Clear();
+            tim[1] -= Time.deltaTime;
+            if(tim[1] <= 0)
+            {
+                b = false;
+                itt.Clear();
+                tim.Clear();
+            }
         }
     }
 }
--- Assets/Developers/Iris/code/Ichor/AttackPatternsIchor/attacks/SweepProjectile.cs
+++ Assets/Developers/Iris/code/Ichor/AttackPatternsIchor/attacks/SweepProjectile.cs
@@ -9,41 +9,50 @@ public class SweepProjectile : MonoBehaviour
 
     private ObjectPoolManager pooler;
     private string prefabName;
+    private float lifespan;
+    private float chargeTimer;
+    private Vector2 vel = new Vector2(0, 0);
+    private bool wallCollision = false;
+    private float speed;
 
-    public float speed;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Update()
     {
-
-    }
-
-    void Update()
-    {
-
+        chargeTimer -= Time.deltaTime;
+        if(chargeTimer <= 0)
+        {
+            rb.velocity = vel;
+        }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        lifespan -= Time.deltaTime;
         if (collision.gameObject.tag == "Player")
         {
-            Vector2 damageDirection = new Vector2(collision.gameObject.transform.position.x - transform.position.x,
-                    collision.gameObject.transform.position.y - transform.position.y);
+            Vector2 damageDirection = new Vector2(collision.gameObject.transform.position.x - transform.position.x, collision.gameObject.transform.position.y - transform.position.y);
             collision.gameObject.GetComponent<PlayerManager>().TakeDamage(damageDirection.normalized, 1, 15);
             pooler.ReleaseObject(prefabName, gameObject);
         }
-        else if (collision.gameObject.layer == 6)
+        else if (collision.gameObject.layer == 6 && wallCollision)
+        {
+            pooler.ReleaseObject(prefabName, gameObject);
+        }
+        else if(lifespan <= 0)
         {
             pooler.ReleaseObject(prefabName, gameObject);
         }
     }
 
-    public void InstantiateComponent(ref ObjectPoolManager poolMan, string prefName, Vector3 pos, Vector3 rot, float dSpeed, float dChargeTimer)
+    public void InstantiateComponent(ref ObjectPoolManager poolMan, string prefName, Vector3 pos, Vector3 rot, float dSpeed, float dChargeTimer, float dLifespan, bool dWallCollision)
     {
         pooler = poolMan;
         prefabName = prefName;
         transform.position = pos;
         transform.rotation = UnityEngine.Quaternion.Euler(rot);
-        rb.velocity = transform.right * speed;
+        rb.velocity = new Vector2(0, 0);
+        vel = transform.right * dSpeed;
+        lifespan = dLifespan;
+        chargeTimer = dChargeTimer;
+        wallCollision = dWallCollision;
     }
 }
