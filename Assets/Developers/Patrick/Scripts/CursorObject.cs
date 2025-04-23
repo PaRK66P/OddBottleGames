c91b8c37d5a3adb6bacd4632834d3afe543c5a20
PaRK66P <2200637@uad.ac.uk>
2025-04-23 14:51:27 +0100

    Cursor fixes
    Turns out the build in cursor doesn't scale with screen size. In most cases with a cursor this is wanted but we have a reticle type cursor so this is not what we want.
    
    The cursor has been changed to a UI object and now scales with screen size and draws over all other UI objects.

---
+++ Assets/Developers/Patrick/Scripts/CursorObject.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorObject : MonoBehaviour
+{
+    [SerializeField]
+    private RectTransform _canvasRect;
+
+    private RectTransform _rectTransform;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rectTransform = GetComponent<RectTransform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 viewportPosition = Camera.main.ScreenToViewportPoint(Input.mousePosition);
+        Vector2 canvasPosition = new Vector2(
+            viewportPosition.x * _canvasRect.sizeDelta.x,
+            viewportPosition.y * _canvasRect.sizeDelta.y);
+
+        _rectTransform.anchoredPosition = canvasPosition;
+    }
+}
