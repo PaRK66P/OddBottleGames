9b9cd85217e05f0c6d96056e7af7efa9f972abce
PaRK66P <2200637@uad.ac.uk>
2025-04-24 14:15:55 +0100

    Menu scene

--- Assets/Developers/Josh/MenuLogic.cs
+++ Assets/Developers/Josh/MenuLogic.cs
@@ -8,21 +8,21 @@ public class MenuLogic : MonoBehaviour
     [Header("Main Menu")]
     [SerializeField] GameObject menuAssets;
 
-    [Header("Settings")]
-    [SerializeField] GameObject SettingsAssets;
-    [SerializeField] Slider volumeSlider;
-    [SerializeField] Slider typingSpeedSlider;
-    [SerializeField] Toggle dashToggle;
-    [SerializeField] Toggle autoTypeToggle;
+    //[Header("Settings")]
+    //[SerializeField] GameObject SettingsAssets;
+    //[SerializeField] Slider volumeSlider;
+    //[SerializeField] Slider typingSpeedSlider;
+    //[SerializeField] Toggle dashToggle;
+    //[SerializeField] Toggle autoTypeToggle;
 
-    [Header("Loading")]
-    [SerializeField] GameObject loadingScreenAssets;
-    [SerializeField] UnityEngine.UI.Image loadingBar;
+    //[Header("Loading")]
+    //[SerializeField] GameObject loadingScreenAssets;
+    //[SerializeField] UnityEngine.UI.Image loadingBar;
 
-    private float volume = 0.3f;
-    private float typingSpeed = 1.0f;
-    private bool dash = false;
-    private bool autoType = true;
+    //private float volume = 0.3f;
+    //private float typingSpeed = 1.0f;
+    //private bool dash = false;
+    //private bool autoType = true;
 
     // Start is called before the first frame update
     public void Start()
@@ -52,25 +52,25 @@ public class MenuLogic : MonoBehaviour
 
     }
 
-    private int BoolToInt(bool val)
-    {
-        if (val == true)
-            return 1;
-        else
-            return 0;
-    }
+    //private int BoolToInt(bool val)
+    //{
+    //    if (val == true)
+    //        return 1;
+    //    else
+    //        return 0;
+    //}
 
-    private bool IntToBool(int val)
-    {
-        if (val == 0)
-            return false;
-        else
-            return true;
-    }
+    //private bool IntToBool(int val)
+    //{
+    //    if (val == 0)
+    //        return false;
+    //    else
+    //        return true;
+    //}
     public void OnStartClick()
     {
         menuAssets.SetActive(false);
-        loadingScreenAssets.SetActive(true);
+        //loadingScreenAssets.SetActive(true);
         StartCoroutine(LoadGame());
         
     }
@@ -80,54 +80,56 @@ public class MenuLogic : MonoBehaviour
         PlayerPrefs.Save();
         AsyncOperation loadGame = SceneManager.LoadSceneAsync("MAP 8");
 
-        while (!loadGame.isDone)
-        {
-            float fillAmount = Mathf.Clamp01(loadGame.progress / 0.9f);
-            loadingBar.fillAmount = fillAmount;
-            yield return null;
-        }
+        yield return null;
+
+        //while (!loadGame.isDone)
+        //{
+        //    float fillAmount = Mathf.Clamp01(loadGame.progress / 0.9f);
+        //    loadingBar.fillAmount = fillAmount;
+        //    yield return null;
+        //}
     }
 
-    public void OnSettingsClick()
-    {
-        menuAssets.SetActive(false);
-        SettingsAssets.SetActive(true);
-    }
+    //public void OnSettingsClick()
+    //{
+    //    menuAssets.SetActive(false);
+    //    SettingsAssets.SetActive(true);
+    //}
 
     public void OnQuitClick()
     {
         Application.Quit();
     }
 
-    public void OnMenuClick()
-    {
-        PlayerPrefs.Save();
-        SettingsAssets.SetActive(false);
-        menuAssets.SetActive(true);
-    }
+    //public void OnMenuClick()
+    //{
+    //    PlayerPrefs.Save();
+    //    SettingsAssets.SetActive(false);
+    //    menuAssets.SetActive(true);
+    //}
 
-    public void OnVolumeSliderChanged(float value)
-    {
-        volume = value;
-        PlayerPrefs.SetFloat("volume", value);
-    }
+    //public void OnVolumeSliderChanged(float value)
+    //{
+    //    volume = value;
+    //    PlayerPrefs.SetFloat("volume", value);
+    //}
 
-    public void OnTextSpeedSliderChange(float value)
-    {
-        typingSpeed = value;
-        PlayerPrefs.SetFloat("typingSpeed", value);
-    }
+    //public void OnTextSpeedSliderChange(float value)
+    //{
+    //    typingSpeed = value;
+    //    PlayerPrefs.SetFloat("typingSpeed", value);
+    //}
 
-    public void OnTypingTextToggleChanged(bool toggle)
-    {
-        autoType = toggle;
-        PlayerPrefs.SetInt("autoType", BoolToInt(toggle));
-    }
+    //public void OnTypingTextToggleChanged(bool toggle)
+    //{
+    //    autoType = toggle;
+    //    PlayerPrefs.SetInt("autoType", BoolToInt(toggle));
+    //}
 
-    public void DashToMouseToggleChanged(bool toggle)
-    {
-        dash = toggle;
-        PlayerPrefs.SetInt("dash", BoolToInt(toggle));
-    }
+    //public void DashToMouseToggleChanged(bool toggle)
+    //{
+    //    dash = toggle;
+    //    PlayerPrefs.SetInt("dash", BoolToInt(toggle));
+    //}
 
 }
